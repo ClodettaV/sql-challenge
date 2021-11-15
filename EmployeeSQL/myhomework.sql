1b67d4e4a28e08f20dae8bbe51901af6e2cb90d5
Claudia <valenticlaudia85@gmail.com>
2021-11-15 16:11:36 +0800

    FINAL

---
+++ EmployeeSQL/myhomework.sql
@@ -0,0 +1,158 @@
+-------------------------------- DATA ENGINIEERING -----------------------------------
+-- Use the information you have to create a table schema for each of the six CSV files. 
+-- Remember to specify data types, primary keys, foreign keys, and other constraints.
+-- Import each CSV file into the corresponding SQL table. 
+
+-- Set Postgres database to see date as 'MDY' permanently
+ALTER DATABASE "Employee_DB" SET datestyle TO "ISO, MDY";
+
+DROP TABLE departments
+
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY NOT NULL,
+	dept_name VARCHAR NOT NULL
+);
+
+SELECT * FROM departments;
+
+	----------------------
+DROP TABLE dept_emp
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+
+	----------------------
+DROP TABLE dept_manager
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM dept_manager;
+
+	-----------------------
+
+DROP TABLE employees CASCADE
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT * FROM employees;
+
+	-----------------------
+	
+DROP TABLE salaries
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+ );
+
+SELECT * FROM salaries;
+
+	-----------------------
+
+DROP TABLE titles
+
+CREATE TABLE titles (
+	title_id VARCHAR PRIMARY KEY NOT NULL,
+	title VARCHAR NOT NULL
+ );
+
+SELECT * FROM titles;
+	
+-------------------------------- DATA ANALYSIS -----------------------------------
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT 
+employees.emp_no, 
+employees.last_name, 
+employees.first_name, 
+employees.sex, 
+salaries.salary
+FROM employees
+JOIN salaries ON 
+employees.emp_no = salaries.emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31';
+
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments 
+LEFT JOIN dept_manager 
+ON departments.dept_no = dept_manager.dept_no
+LEFT JOIN employees 
+ON dept_manager.emp_no = employees.emp_no;
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp 
+LEFT JOIN departments 
+ON dept_emp.dept_no = departments.dept_no
+LEFT JOIN employees
+ON dept_emp.emp_no = employees.emp_no;
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE employees.first_name = 'Hercules' 
+AND employees.last_name like 'B%';
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments
+ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales'
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments
+ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' 
+OR departments.dept_name = 'Development'
+
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(*)
+as "frequency_of_last_names"
+from employees
+group by last_name
+order by "frequency_of_last_names"
+
+
+
+
